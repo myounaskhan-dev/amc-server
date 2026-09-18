17874fdc6d3a35f2a34180b75e4fb1582ffbc7ec
unknown <your-email@gmail.com>
2026-09-18 06:18:46 -0700

    fix vercel server

--- server.js
+++ server.js
@@ -13,23 +13,21 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/products", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("Ali Mobile Corner Server is running!");
 });
 
+app.use("/api/products", productRoutes);
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected successfully");
-
-    const PORT = process.env.PORT || 5000;
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
   })
   .catch((error) => {
     console.error("MongoDB connection error:", error.message);
   });
+
+export default app;
+
