bfe19594042656f8e722307fc83dedc7d9ee71b5
unknown <your-email@gmail.com>
2026-09-18 06:46:30 -0700

    fix vercel server export

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
+
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import dns from "dns";
 import productRoutes from "./routes/ProductRoutes.js";
 
 dotenv.config();
-
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const app = express();
 
@@ -20,14 +18,20 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected successfully");
-  })
-  .catch((error) => {
-    console.error("MongoDB connection error:", error.message);
-  });
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  console.error("MONGO_URI is missing");
+} else {
+  mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+      console.log("MongoDB connected successfully");
+    })
+    .catch((error) => {
+      console.error("MongoDB connection error:", error.message);
+    });
+}
 
 export default app;
 
